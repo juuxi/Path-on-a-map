56a339abf2e223ce3396ee50e15b283867117b25
Dmitry Shopyrev <dimashopyrev@mail.ru>
2025-03-31 14:57:15 +0300

    Map add/remove obstacles

--- map.cpp
+++ map.cpp
@@ -4,6 +4,16 @@ Map::Map() {
 
 }
 
+void Map::AddObstacle(Obstacle* _obstacle) {
+    obstacles.push_back(_obstacle);
+}
+
+void Map::RemoveObstacle(Obstacle* _obstacle) {
+    if (obstacles.indexOf(_obstacle) != -1) {
+        obstacles.remove(obstacles.indexOf(_obstacle));
+    }
+}
+
 Map::~Map() {
     while (!obstacles.empty()) {
         delete obstacles.front();
